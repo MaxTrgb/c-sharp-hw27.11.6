a759d756ecc9b1b5ec0363298aeaacbf339127fb
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2023-11-26 16:20:00 +0200

    Add files via upload

---
+++ C#hw27.11.5/C#hw27.11.5/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MySpace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the lower limit of the range:");
+            int lowerLimit = GetValidNumberInput();
+
+            Console.WriteLine("Enter the upper limit of the range:");
+            int upperLimit = GetValidNumberInput();
+
+            Console.WriteLine($"\nFibonacci numbers in the range {lowerLimit} - {upperLimit}:");
+
+            int first = 0, second = 1;
+            while (first <= upperLimit)
+            {
+                if (first >= lowerLimit)
+                {
+                    Console.Write(first + " ");
+                }
+
+                int temp = first + second;
+                first = second;
+                second = temp;
+            }
+        }
+
+        private static int GetValidNumberInput()
+        {
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number:");
+            }
+            return number;
+        }
+    }
+}
